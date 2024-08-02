fd2107fb846efde1e3732e5be6611e3c5ded7a35
Andrej Kolchin <KAAtheWise@protonmail.com>
2024-08-02 20:40:00 +0300

    [endecode] z85: move to the `command` module

--- endecode/src/command/mod.rs
+++ endecode/src/command/mod.rs
@@ -1,5 +1,7 @@
 mod html;
 mod z32;
+mod z85;
 
 pub use html::{HtmlDecode, HtmlEncode};
 pub use z32::{Z32Decode, Z32Encode};
+pub use z85::{Z85Decode, Z85Encode};
--- endecode/src/command/z85.rs
+++ endecode/src/command/z85.rs
@@ -2,13 +2,13 @@ use nu_plugin::{EngineInterface, EvaluatedCall, PluginCommand};
 use nu_protocol::{
 	IntoPipelineData, LabeledError, PipelineData, Signature, Value,
 };
-use z85::decode;
+use z85::{decode, encode};
 
 use crate::EndecodePlugin;
 
-pub struct Z85;
+pub struct Z85Decode;
 
-impl PluginCommand for Z85 {
+impl PluginCommand for Z85Decode {
 	type Plugin = EndecodePlugin;
 
 	fn name(&self) -> &str {
@@ -42,5 +42,39 @@ impl PluginCommand for Z85 {
 			Err(err) => Err(LabeledError::new("Decoding error")
 				.with_label(err.to_string(), span)),
 		}
+	}
+}
+
+pub struct Z85Encode;
+
+impl PluginCommand for Z85Encode {
+	type Plugin = EndecodePlugin;
+
+	fn name(&self) -> &str {
+		"encode z85"
+	}
+
+	fn usage(&self) -> &str {
+		"Encode a string or binary value using ZeroMQ’s Z85 encoding."
+	}
+
+	fn signature(&self) -> Signature {
+		crate::util::encode_signature(self.name())
+	}
+
+	fn search_terms(&self) -> Vec<&str> {
+		vec!["encoding"]
+	}
+
+	fn run(
+		&self,
+		_plugin: &EndecodePlugin,
+		_engine: &EngineInterface,
+		call: &EvaluatedCall,
+		input: PipelineData,
+	) -> Result<PipelineData, LabeledError> {
+		let (bytes, _) = crate::util::get_bytes(input, call.head)?;
+		let out = encode(&bytes);
+		Ok(Value::string(out, call.head).into_pipeline_data())
 	}
 }
