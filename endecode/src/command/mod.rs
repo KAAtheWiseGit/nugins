0747ee189adee26f8d9e8ff374f4241e5a373f79
Andrej Kolchin <KAAtheWise@protonmail.com>
2024-08-02 20:44:00 +0300

    [endecode] base58: move to the `command` module

--- endecode/src/command/mod.rs
+++ endecode/src/command/mod.rs
@@ -2,8 +2,10 @@ mod html;
 mod z32;
 mod z85;
 mod crockford;
+mod base58;
 
 pub use html::{HtmlDecode, HtmlEncode};
 pub use z32::{Z32Decode, Z32Encode};
 pub use z85::{Z85Decode, Z85Encode};
 pub use crockford::{CrockfordDecode, CrockfordEncode};
+pub use base58::{Base58Decode, Base58Encode};
