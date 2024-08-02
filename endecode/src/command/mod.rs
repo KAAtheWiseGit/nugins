5e0608001d81a6a862ad3599db3a48e77c2073bf
Andrej Kolchin <KAAtheWise@protonmail.com>
2024-08-02 21:25:52 +0300

    [endecode] command: format

--- endecode/src/command/mod.rs
+++ endecode/src/command/mod.rs
@@ -1,11 +1,11 @@
+mod base58;
+mod crockford;
 mod html;
 mod z32;
 mod z85;
-mod crockford;
-mod base58;
 
+pub use base58::{Base58Decode, Base58Encode};
+pub use crockford::{CrockfordDecode, CrockfordEncode};
 pub use html::{HtmlDecode, HtmlEncode};
 pub use z32::{Z32Decode, Z32Encode};
 pub use z85::{Z85Decode, Z85Encode};
-pub use crockford::{CrockfordDecode, CrockfordEncode};
-pub use base58::{Base58Decode, Base58Encode};
