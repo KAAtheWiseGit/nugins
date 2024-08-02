ad3abf5bb27a545ea9642396ab2ae68a70ea0a0b
Andrej Kolchin <KAAtheWise@protonmail.com>
2024-08-02 20:42:14 +0300

    [endecode] crockford: move to the `command` module

--- endecode/src/command/crockford.rs
+++ endecode/src/command/crockford.rs
@@ -6,9 +6,9 @@ use nu_protocol::{
 
 use crate::EndecodePlugin;
 
-pub struct Crockford;
+pub struct CrockfordEncode;
 
-impl PluginCommand for Crockford {
+impl PluginCommand for CrockfordEncode {
 	type Plugin = EndecodePlugin;
 
 	fn name(&self) -> &str {
@@ -45,5 +45,40 @@ impl PluginCommand for Crockford {
 					span,
 				)),
 		}
+	}
+}
+
+pub struct CrockfordEncode;
+
+impl PluginCommand for CrockfordEncode {
+	type Plugin = EndecodePlugin;
+
+	fn name(&self) -> &str {
+		"encode crockford"
+	}
+
+	fn usage(&self) -> &str {
+		"Encode a string or binary value using Base32, RFC 4648, section 6."
+	}
+
+	fn signature(&self) -> Signature {
+		crate::util::encode_signature(self.name())
+	}
+
+	fn search_terms(&self) -> Vec<&str> {
+		vec!["encoding", "base32"]
+	}
+
+	fn run(
+		&self,
+		_plugin: &EndecodePlugin,
+		_engine: &EngineInterface,
+		call: &EvaluatedCall,
+		input: PipelineData,
+	) -> Result<PipelineData, LabeledError> {
+		let (bytes, _) = crate::util::get_bytes(input, call.head)?;
+		let out = encode(Alphabet::Crockford, &bytes);
+
+		Ok(Value::string(out, call.head).into_pipeline_data())
 	}
 }
